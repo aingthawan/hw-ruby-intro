553fad06e204a70987eedda8d1c3b5119679765d
aingez <s6401012610042@email.kmutnb.ac.th>
2023-07-23 19:40:24 +0700

    edit docker file & upload existed work
    docker cannot start since Ruby 2.6.6 causing a problem, Then switched to version 3.0

--- lib/ruby_intro.rb
+++ lib/ruby_intro.rb
@@ -3,33 +3,70 @@
 # Part 1
 
 def sum arr
-  # YOUR CODE HERE
+  # if array is empty return 0
+  return 0 if arr.empty?
+  # if not, return result of sum
+  arr.sum
 end
 
 def max_2_sum arr
-  # YOUR CODE HERE
+  return 0 if arr.empty?
+  # if have one element return that one
+  return arr[0] if arr.length == 1
+  # if not, return sum of two largest elements
+  arr.max(2).sum
 end
 
 def sum_to_n? arr, n
-  # YOUR CODE HERE
+  # if array is empty return false
+  return false if arr.empty?
+  # if not, return true if any two elements sum to n
+  arr.combination(2).any? { |a, b| a + b == n }
 end
 
 # Part 2
 
 def hello(name)
-  # YOUR CODE HERE
+  return "Hello, #{name}"
 end
 
-def starts_with_consonant? s
-  # YOUR CODE HERE
+def starts_with_consonant?(s)
+  # return false if s.empty?
+  if s.empty?
+    return false
+  elsif s =~ /^[^aeiou\W]/i
+    return true
+  else
+    return false
+  end
 end
 
 def binary_multiple_of_4? s
-  # YOUR CODE HERE
+   # return true if multiple of 4
+   if s =~ /^[10]*00$/ || s == '0'
+    return true
+  else
+    return false
+  end
 end
 
 # Part 3
 
 class BookInStock
-# YOUR CODE HERE
+  attr_accessor :isbn, :price
+
+  def initialize(isbn, price)
+    if isbn.empty? || price <= 0
+      raise ArgumentError, "ISBN number is the empty string or if the price is less than or equal to zero!"
+    end
+
+    @isbn = isbn
+    @price = price
+  end
+
+  # method price_as_string,
+  # ex, 33.8 => $33.80
+  def price_as_string
+    "$#{'%.2f' % @price}"
+  end
 end
